669f40878cd20c22408cebf31e58547398196a7e
b.paliak <b.paliak@gmail.com>
2014-03-30 10:47:50 +0530

    firtst commit

---
+++ Telematics/Telematics-core/src/main/java/com/tcs/AppCore.java
@@ -0,0 +1,20 @@
+package com.tcs;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppCore 
+{
+	
+	void test(){
+		
+		
+		   System.out.println( "Core test" );
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
