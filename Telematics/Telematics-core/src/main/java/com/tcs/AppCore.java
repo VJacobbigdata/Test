efd6b9ac93b2f24562ae1acd95166bf01aae9ac9
b.paliak <b.paliak@gmail.com>
2014-03-30 11:48:33 +0530

    test

---
+++ Telematics/Telematics-core/src/main/java/com/tcs/AppCore.java
@@ -0,0 +1,20 @@
+package com.tcs;
+
+/**
+ * Hello world! tjs is a new file
+ *
+ */
+public class AppCore 
+{
+	
+	void test(){
+		
+		
+		   System.out.println( "Core test" );
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Telematics/Telematics-eod/src/main/java/com/tcs/AppEod.java
@@ -0,0 +1,13 @@
+package com.tcs;
+
+/**
+ * Hello world! this ia s master file
+ * after the creation of testing bracnch move it to testing branch
+ */
+public class AppEod 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
